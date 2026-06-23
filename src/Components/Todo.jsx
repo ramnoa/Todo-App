48919af8aca1edfc6d3ec8c02a92ba50c31c14a3
ramnoa <ramzeynoah11@gmail.com>
2026-06-23 11:30:28 +0300

    Submitting The To do items

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
 export default function Todo() {
     const [todo, setTodo] = useState("");
-    
+    const [todos, setTodos] = useState([]);
+    function  handleSubmit(e){
+        e.preventDefault();
+        setTodos([...todos, todo]);
+
+        
+    }
     return(<div>
-        <form>
+        <form onSubmit={handleSubmit}>
             <input onChange ={(e) => setTodo(e.target.value)} value={todo} type="text" />
-            <button type="submit">Add</button>
+            <button  type="submit">Add</button>
             
         </form>
+           {console.log(todos)}
     </div>)
 }
