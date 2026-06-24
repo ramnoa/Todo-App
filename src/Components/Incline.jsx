9f5adc7cdb3da8ff152d38ff1fa0f92d388f5ace
ramnoa <ramzeynoah11@gmail.com>
2026-06-24 06:52:23 +0300

    CSS Stylesheets

--- src/Components/Incline.jsx
+++ src/Components/Incline.jsx
@@ -6,10 +6,18 @@
 //     )}
 
 //The 2nd way of doing inline styling is to create a variable and then use it in the style attribute. This is useful when you have multiple styles to apply or when you want to reuse the same styles in different components.
-    const header  = {color: "green" , fontSize:"70px"}
+    // const header  = {color: "green" , fontSize:"70px"}
+    // export default function   Inline() {
+    // return(
+    //     <h1 style={header}>
+    //         inline styling
+    //     </h1>
+    // )}\
+    import "../css/style.css"
+     const header  = {color: "green" , fontSize:"70px"}
     export default function   Inline() {
     return(
-        <h1 style={header}>
+        <h1 className="header">
             inline styling
         </h1>
     )}
